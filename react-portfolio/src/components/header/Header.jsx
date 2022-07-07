fb15979eab673988b9622871c1715863ecd14bb2
benntuecon <zxc123598tw@gmail.com>
2022-07-06 22:03:41 -0700

    init header

--- react-portfolio/src/components/header/Header.jsx
+++ react-portfolio/src/components/header/Header.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
 
 const Header = () => {
     return (
-        <div>header</div>
+        <header>
+            <div className="container header__container">
+                <h5>
+                    HELLO, I'M
+                </h5>
+                <h1>
+                    Ben Chen
+                </h1>
+                <h5 className='text-light'>
+                    A data scientist and web developer
+                </h5>
+                <CTA />
+                <div className='me'>
+                    <img src={ME} alt='me' />
+                </div>
+
+                <a href='#contact' className='scroll__down'> Scroll Down</a>
+            </div>
+
+        </header >
     )
 }
 
