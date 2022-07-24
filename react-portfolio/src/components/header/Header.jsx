2fde9d0014aa09eb08b9d7387503cc242ced2e00
benntuecon <zxc123598tw@gmail.com>
2022-07-24 13:41:55 -0700

    finish header v1

--- react-portfolio/src/components/header/Header.jsx
+++ react-portfolio/src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
     return (
@@ -17,6 +18,7 @@ const Header = () => {
                     A data scientist and web developer
                 </h5>
                 <CTA />
+                <HeaderSocials />
                 <div className='me'>
                     <img src={ME} alt='me' />
                 </div>
