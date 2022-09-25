fc01b57654773efdd903a14867c7eb9471535937
benntuecon <zxc123598tw@gmail.com>
2022-09-25 16:16:21 -0700

    nav done

--- react-portfolio/src/components/nav/nav.jsx
+++ react-portfolio/src/components/nav/nav.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import './nav.css'
+import { AiOutlineHome } from 'react-icons/ai'
+import { FaUserTie } from 'react-icons/fa'
+import { IoPlanetSharp } from 'react-icons/io5'
+import { AiFillTool } from 'react-icons/ai'
+import { AiFillMessage } from 'react-icons/ai'
+import { useState } from 'react'
+
+
 
 const Nav = () => {
+    const [activeNav, setActiveNav] = useState('#')
     return (
-        <div>nav</div>
+
+        <nav>
+            <a href="#" onClick={() => { setActiveNav('#') }} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+            <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FaUserTie /></a>
+            <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}> <IoPlanetSharp /></a>
+            <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><AiFillTool /></a>
+            <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}> <AiFillMessage /></a>
+        </nav >
     )
 }
 
